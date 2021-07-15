f46fce7d67805f5667f3c53cf33e3d752c6726d0
k1999-darouich <k.darouich_etu@enset-media.ac.ma>
2021-07-15 15:43:07 +0100

    addChatEngine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import  ChatFeed from './components/ChatFeed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=> {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="d42b0d1e-7845-4a95-b5cd-266ff7b56e64"
+            userName="k-darouich"
+            userSecret="R130182235"
+            renderChatFeed= { (chatAppProps) => <ChatFeed {...chatAppProps}/> }
+
+        />
+
+        
+    )
 }
 
 export default App;
